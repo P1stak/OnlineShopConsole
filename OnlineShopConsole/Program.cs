874088dff46c55b313f19dabe12c5a1d5bfb9b17
Александр Pistak <pistak@deeplay.io>
2025-04-10 12:49:03 +0700

    Добавление функционала администратора и пользователя

--- OnlineShopConsole/Program.cs
+++ OnlineShopConsole/Program.cs
@@ -39,14 +39,41 @@ namespace OnlineShopConsole
 
 
         }
+        public class User
+        {
+            public string Name;
+            public List<Order> Orders;
+            public User(string name)
+            {
+                Name = name;
+                Orders = new List<Order>();
+            }
+            public void ShowOrders()
+            {
+                Console.WriteLine($"Заказы пользователя {Name}:\n");
+                int orderNum = 1;
+                foreach (var order in Orders)
+                {
+                    Console.WriteLine($"Заказ {orderNum++}:");
+                    foreach (var product in order.Products)
+                    {
+                        product.Print();
+                    }
+                    Console.WriteLine($"Общая стоимость: {order.FullPrice}\n");
+                }
+            }
+        }
         public class Store
         {
             public List<Product> Products;
             public List<Product> Basket; // корзина
             public List<Order> Orders;
+            private User currentUser;
 
-            public Store()
+
+            public Store(User user)
             {
+                currentUser = user;
                 Products = new List<Product> 
                 {
                     new Product("Хлеб", 50),
@@ -86,75 +113,162 @@ namespace OnlineShopConsole
             }
             public void AddToBasket(int numberProduct)
             {
-                Basket.Add(Products[numberProduct - 1]);
-                Console.WriteLine($"Продукт {Products[numberProduct - 1].Name} успешно добавлен в корзину");
-                Console.WriteLine($"В корзине {Basket.Count} продуктов");
+                if (numberProduct > 0 && numberProduct <= Products.Count)
+                {
+                    Basket.Add(Products[numberProduct - 1]);
+                    Console.WriteLine($"Продукт {Products[numberProduct - 1].Name} успешно добавлен в корзину");
+                    Console.WriteLine($"В корзине {Basket.Count} продуктов");
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный номер продукта.");
+                }
             }
 
-            internal void CreateOrder()
+            public void CreateOrder()
             {
-                // передать от отдел доставки
-                Order order = new Order(Basket);
-                Orders.Add(order);
-                Basket.Clear();
+                // передать в отдел доставки
+                if (Basket.Count > 0)
+                {
+                    Order order = new Order(Basket);
+                    Orders.Add(order);
+                    currentUser.Orders.Add(order);
+                    Basket.Clear();
+                    Console.WriteLine($"Заказ успешно создан на сумму {order.FullPrice}");
+                }
+                else
+                {
+                    Console.WriteLine("Корзина пуста. Добавьте товары перед оформлением заказа.");
+                }
+            }
+            public void AddProductToCatalog(string name, decimal price)
+            {
+                Products.Add(new Product(name, price));
+                Console.WriteLine($"Продукт {name} успешно добавлен в каталог.");
+            }
+
+            public void RemoveProduct(int productNum)
+            {
+                if (productNum > 0 && productNum <= Products.Count)
+                {
+                    Products.RemoveAt(productNum - 1);
+                    Console.WriteLine($"Продукт {productNum} успешно удален из каталога.");
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный номер продукта.");
+                }
             }
         }
         static void Main(string[] args)
         {
-            Store onlineStore = new Store();
-            
-            Console.WriteLine("Здравствуйте. Выберите действие:");
-            Console.WriteLine("1. Показать каталог продуктов?");
-            Console.WriteLine("Выберите номер действия, которое хотите совершить.");
+            // создали логин юзера для заказа
+            Console.WriteLine("Введите ваше имя:");
+            var userName = Console.ReadLine();
+            User currentUser = new User(userName);
+            Store onlineStore = new Store(currentUser);
 
-            var numbeberAction = int.Parse(Console.ReadLine());
-            switch (numbeberAction)
-            {
-                case 1:
-                    onlineStore.ShowCatalog();
-                    break;
-                case 2: 
 
-                default:
-                    Console.WriteLine("Выберите номер действия из списка");
-                    break;
-            }
-            bool yes = false;
-            do
+            bool continueProgram = true;
+            while (continueProgram)
             {
-                Console.WriteLine("Хотите добавить новый продукт в корзину? Напишите ДА или НЕТ");
-                yes = IsYes(Console.ReadLine());
-                if (yes)
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Показать каталог");
+                Console.WriteLine("2. Добавить продукт в корзину");
+                Console.WriteLine("3. Оформить заказ");
+                Console.WriteLine("4. Добавить новый продукт в каталог (администратор)");
+                Console.WriteLine("5. Удалить продукт в каталоге (администратор)");
+                Console.WriteLine("6. Показать заказы пользователя");
+                Console.WriteLine("7. Выйти");
+
+                if (int.TryParse(Console.ReadLine(), out int numberAction))
                 {
-                    onlineStore.ShowCatalog();
-                    Console.WriteLine("Напиишите номер продукта, которого нужно добавить в корзину");
-                    var productNumber = int.Parse(Console.ReadLine());
-                    onlineStore.AddToBasket(productNumber);
+                    switch (numberAction)
+                    {
+                        case 1:
+                            onlineStore.ShowCatalog();
+                            break;
+
+                        case 2:
+                            bool yes = false;
+                            do
+                            {
+                                Console.WriteLine("Хотите добавить новый продукт в корзину? Напишите ДА или НЕТ");
+                                yes = IsYes(Console.ReadLine());
+                                if (yes)
+                                {
+                                    onlineStore.ShowCatalog();
+                                    Console.WriteLine("Напишите номер продукта, которого нужно добавить в корзину");
+                                    if (int.TryParse(Console.ReadLine(), out int productNumber))
+                                    {
+                                        onlineStore.AddToBasket(productNumber);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Некорректный номер продукта.");
+                                    }
+                                }
+                            }
+                            while (yes);
+                            Console.WriteLine("Хотите посмотреть корзину с добавленными продуктами? Напишите ДА или НЕТ");
+                            yes = IsYes(Console.ReadLine());
+                            if (yes)
+                            {
+                                onlineStore.ShowBasket();
+                            }
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Хотите оформить заказ? Напишите ДА или НЕТ");
+                            yes = IsYes(Console.ReadLine());
+                            if (yes)
+                            {
+                                onlineStore.CreateOrder();
+                            }
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Введите название нового продукта");
+                            var productName = Console.ReadLine();
+                            Console.WriteLine("Введите цену нового продукта");
+                            if (decimal.TryParse(Console.ReadLine(), out decimal productPrice))
+                            {
+                                onlineStore.AddProductToCatalog(productName, productPrice);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректная цена продукта.");
+                            }
+                                break;
+                        case 5:
+                            onlineStore.ShowCatalog();
+                            Console.WriteLine("Введите номер продукта, который хотите удалить");                           
+                            int productNum = int.Parse(Console.ReadLine());
+                            onlineStore.RemoveProduct(productNum);
+
+                            break;
+                        case 6:
+                            currentUser.ShowOrders();
+
+                            break;
+                        case 7:
+                            continueProgram = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Выберите номер действия из списка");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
                 }
             }
-            while (yes);
-
-            Console.WriteLine("Хотите посмотреть корзину с добавленными продуктами? Напишите ДА или НЕТ");
-            yes = IsYes(Console.ReadLine());
-            if (yes)
-            {
-                onlineStore.ShowBasket();
-            }
-
-            Console.WriteLine("Хотите оформить заказ? Напишите ДА или НЕТ");
-            yes = IsYes(Console.ReadLine());
-            if (yes)
-            {
-                onlineStore.CreateOrder();
-            }
         }
-        // Сделать меню, то есть дать пользователю выбирать какое действие он хочет совершить. Предусмотреть актуальность действий в меню. 
 
-        // Защита от дурака. Проверять все входные данные от пользователя. Ваша задача дать обратную связь пользователю о его неудачных действиях. 
 
-        // Создать класс пользователя, в котором будет его имя и список его заказов. 
-
-        // Возможности администратора: добавление нового продукта.
+        // Возможности администратора: редактирование цены продуктов!
 
         static bool IsYes(string answer)
         {
